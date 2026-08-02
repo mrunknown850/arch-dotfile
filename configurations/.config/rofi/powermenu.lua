1be2764846f7308f8cb200750912cbf00b76e6a7
Đặng Trần Quang Minh <minh0608@outlook.com>
2026-08-02 23:04:56 +0700

    Add hyprshutdown support for graceful ACPI.

--- configurations/.config/rofi/powermenu.lua
+++ configurations/.config/rofi/powermenu.lua
@@ -3,39 +3,39 @@
 -- Use a list because table keys are randomly sorted
 -- It's also easier to add options or edit them this way
 local options = {
-  {
-    name = "Sleep",
-    icon = "system-suspend",
-    command = "systemctl suspend"
-  },
-  {
-    name = "Shut down",
-    icon = "system-shutdown",
-    command = "systemctl poweroff"
-  },
-  {
-    name = "Restart",
-    icon = "system-reboot",
-    command = "systemctl reboot"
-  },
-  {
-    name = "Lock",
-    icon = "system-lock-screen",
-    command = "hyprlock"
-  },
-  {
-    name = "Log out",
-    icon = "system-log-out",
-    command = "hyprctl dispatch 'hl.dsp.exit()'"
-  },
+	{
+		name = "Sleep",
+		icon = "system-suspend",
+		command = "systemctl suspend",
+	},
+	{
+		name = "Shut down",
+		icon = "system-shutdown",
+		command = "hyprshutdown -t 'Shutting down...' -p 'systemctl poweroff'",
+	},
+	{
+		name = "Restart",
+		icon = "system-reboot",
+		command = "hyprshutdown -t 'Rebooting...' -p 'systemctl reboot'",
+	},
+	{
+		name = "Lock",
+		icon = "system-lock-screen",
+		command = "hyprlock",
+	},
+	{
+		name = "Log out",
+		icon = "system-log-out",
+		command = "hyprshutdown -t 'Logging out...'",
+	},
 }
 
 for i, opt in ipairs(options) do
-  if arg[1] then
-    if opt.name == arg[1] then
-      os.execute(opt.command)
-    end
-  else
-    print(opt.name..'\0icon\x1f'..opt.icon)
-  end
+	if arg[1] then
+		if opt.name == arg[1] then
+			os.execute(opt.command)
+		end
+	else
+		print(opt.name .. "\0icon\x1f" .. opt.icon)
+	end
 end
